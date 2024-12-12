bbc737b7301c8f050320773a486a01b404746978
JoelVerastegui <jverasteguixyz@gmail.com>
2024-12-12 11:06:57 -0500

    Fiveth version side B

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,98 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:cinemapedia/presentation/providers/providers.dart';
+
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
+import 'package:cinemapedia/presentation/views/views.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = 'home_screen';
+  final int pageIndex;
 
-  const HomeScreen({super.key});
+  const HomeScreen({
+    super.key, 
+    required this.pageIndex
+  });
 
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
-    );
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-  }
+  final homeViews = const <Widget>[
+    HomeView(),
+    SizedBox(),
+    FavoritesView()
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final initialLoading = ref.watch(initialLoadingProvider);
-    if(initialLoading) return const FullscreenLoader();
-
-    final slideShowMovies = ref.watch(movieSlideshowProvider);
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-
-    return CustomScrollView(
-      slivers: [
-        const SliverAppBar(
-          floating: true,
-          flexibleSpace: CustomAppbar()
-        ),
-
-        SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) {
-            return Column(
-              children: [
-                MovieSlideshow(movies: slideShowMovies),
-            
-                MovieHorizontalListview(
-                  title: 'Now playing',
-                  subtitle: 'February 12',
-                  movies: nowPlayingMovies, 
-                  loadNextPage: () => ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()
-                ),
-            
-                MovieHorizontalListview(
-                  title: 'Popular',
-                  // subtitle: 'Muy pronto',
-                  movies: popularMovies, 
-                  loadNextPage: () => ref.read(popularMoviesProvider.notifier).loadNextPage()
-                ),
-            
-                MovieHorizontalListview(
-                  title: 'Top Rated',
-                  // subtitle: 'Febrero 08',
-                  movies: topRatedMovies, 
-                  loadNextPage: () => ref.read(topRatedMoviesProvider.notifier).loadNextPage()
-                ),
-            
-                MovieHorizontalListview(
-                  title: 'Upcoming',
-                  subtitle: 'Coming soon',
-                  movies: upcomingMovies, 
-                  loadNextPage: () => ref.read(upcomingMoviesProvider.notifier).loadNextPage()
-                ),
-
-                const SizedBox(height: 10)
-              ],
-            );
-          },
-          childCount: 1)
-        )
-      ],
+    return Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: homeViews,
+      ),
+      bottomNavigationBar: CustomBottomNavigation(currentIndex: pageIndex),
     );
   }
 }
---
+++ lib/presentation/views/movies/home_view.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
+
+class HomeView extends ConsumerStatefulWidget {
+  const HomeView({ super.key });
+
+  @override
+  HomeViewState createState() => HomeViewState();
+}
+
+class HomeViewState extends ConsumerState<HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+    ref.read(popularMoviesProvider.notifier).loadNextPage();
+    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
+    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final initialLoading = ref.watch(initialLoadingProvider);
+    if(initialLoading) return const FullscreenLoader();
+
+    final slideShowMovies = ref.watch(movieSlideshowProvider);
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final popularMovies = ref.watch(popularMoviesProvider);
+    final topRatedMovies = ref.watch(topRatedMoviesProvider);
+    final upcomingMovies = ref.watch(upcomingMoviesProvider);
+
+    return CustomScrollView(
+      slivers: [
+        const SliverAppBar(
+          floating: true,
+          flexibleSpace: CustomAppbar()
+        ),
+
+        SliverList(
+          delegate: SliverChildBuilderDelegate((context, index) {
+            return Column(
+              children: [
+                MovieSlideshow(movies: slideShowMovies),
+            
+                MovieHorizontalListview(
+                  title: 'Now playing',
+                  subtitle: 'February 12',
+                  movies: nowPlayingMovies, 
+                  loadNextPage: () => ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()
+                ),
+            
+                MovieHorizontalListview(
+                  title: 'Popular',
+                  // subtitle: 'Muy pronto',
+                  movies: popularMovies, 
+                  loadNextPage: () => ref.read(popularMoviesProvider.notifier).loadNextPage()
+                ),
+            
+                MovieHorizontalListview(
+                  title: 'Top Rated',
+                  // subtitle: 'Febrero 08',
+                  movies: topRatedMovies, 
+                  loadNextPage: () => ref.read(topRatedMoviesProvider.notifier).loadNextPage()
+                ),
+            
+                MovieHorizontalListview(
+                  title: 'Upcoming',
+                  subtitle: 'Coming soon',
+                  movies: upcomingMovies, 
+                  loadNextPage: () => ref.read(upcomingMoviesProvider.notifier).loadNextPage()
+                ),
+
+                const SizedBox(height: 10)
+              ],
+            );
+          },
+          childCount: 1)
+        )
+      ],
+    );
+  }
+}
